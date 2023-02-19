41c40e4d1522dc71b254d32c97c3781250f687e0
ivalchanka <i.valchenk@hotmail.com>
2023-02-19 09:54:46 +0400

    Added project skeleton.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private bool isSpaceBarDisabled = false;
+    public GameObject dogPrefab;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ToggleSpaceBar", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!this.isSpaceBarDisabled)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+    }
+
+    public void ToggleSpaceBar()
+    {
+        this.isSpaceBarDisabled = !this.isSpaceBarDisabled;
+    }
+}
